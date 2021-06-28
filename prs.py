947a40fd2761daf501c03ed7ab9d6973a0f5384a
bilie125 <53867216+bilie125@users.noreply.github.com>
2021-06-29 00:29:45 +0300

    Add files via upload

---
+++ prs.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+import requests
+import imgkit
+URL = 'https://phdpu.edu.ua/course/oo-11pm-e-12pm-f-13pm-pv/'
+page = requests.get(URL)
+  # load the page content
+text = page.content
+  
+soup = BeautifulSoup(text, "html.parser")
+with open("D:\Project\parser\output.html", "w", encoding = 'utf-8') as file:
+    
+    # prettify the soup object and convert it into a string
+    file.write(str(soup.table.prettify()))
+
+css = 'style.css'
+
+imgkit.from_file('output.html','out.jpg', css=css)
